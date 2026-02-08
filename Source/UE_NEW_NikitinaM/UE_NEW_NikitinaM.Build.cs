16ac938ab7480bfb16a81df2d1e4599a5e57e0f8
marioswin11-a11y <mari.oswin11@gmail.com>
2026-02-07 21:58:56 -0700

    GAS: Enable plugins+add modules

--- Source/UE_NEW_NikitinaM/UE_NEW_NikitinaM.Build.cs
+++ Source/UE_NEW_NikitinaM/UE_NEW_NikitinaM.Build.cs
@@ -8,6 +8,6 @@ public class UE_NEW_NikitinaM : ModuleRules
 	{
 		PCHUsage = PCHUsageMode.UseExplicitOrSharedPCHs;
 
-		PublicDependencyModuleNames.AddRange(new string[] { "Core", "CoreUObject", "Engine", "InputCore", "EnhancedInput" });
+		PublicDependencyModuleNames.AddRange(new string[] { "Core", "CoreUObject", "Engine", "InputCore", "EnhancedInput", "GameplayAbilities", "GameplayTags", "GameplayTasks" });
 	}
 }
